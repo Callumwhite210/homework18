e9d09451452db312b1b875e79742ac5b0ca11db9
Callum White <callumwhite210@gmail.com>
2020-12-28 16:09:42 +1030

    setting heroku dynamic port

--- server.js
+++ server.js
@@ -15,12 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/onlineOfflineTracker";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/onlineOfflineTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 
 // mongoose.connect("mongodb://localhost/onlineOfflineTracker", {
